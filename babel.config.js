1a3d6d702ee793f69d2dfcdba76fccba8074f1dd
Sunil Parekh <sunil@we3.io>
2023-09-26 18:24:08 +0100

    fix: enable web rendering

--- babel.config.js
+++ babel.config.js
@@ -1,18 +1,19 @@
 module.exports = function (api) {
-	api.cache(true);
-	return {
-		presets: ["babel-preset-expo"],
-		plugins: [
-			require.resolve("expo-router/babel"),
-			[
-				"module-resolver",
-				{
-					alias: {
-						"@": ".",
-					},
-				},
-			],
-			"react-native-reanimated/plugin",
-		],
-	};
+  api.cache(true);
+  return {
+    presets: ["babel-preset-expo"],
+    plugins: [
+      require.resolve("expo-router/babel"),
+      [
+        "module-resolver",
+        {
+          alias: {
+            "@": ".",
+          },
+        },
+      ],
+      "@babel/plugin-transform-export-namespace-from",
+      "react-native-reanimated/plugin",
+    ],
+  };
 };
